fd6c289aca7bd825d0e7a1097b27343a2f88fdb5
Crimson17 <46162167+Crimson17@users.noreply.github.com>
2022-04-03 20:23:42 +0200

    gitignore fixes

---
+++ Engine/projects/Test/scripts/Move.java
@@ -0,0 +1,17 @@
+import engine.*;
+import engine.math.*;
+import engine.script.*;
+
+
+public class Move extends Entity implements Script {
+
+	// Called on first frame
+	public void start() {
+
+	}
+
+	// Called every frame
+	public void update() {
+		this.position.x = (float)Math.sin(Engine.elapsedT) * 5.0f;
+	}
+}
